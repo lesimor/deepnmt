eab149cf9be8dccae9232bb5268df0aeee5169cc
Byungwook Kang <byungwook@ByungWook-M1-pro.local>
2022-05-02 10:55:21 +0900

    Use only multi gpu

--- run_wmt14_en_fr.sh
+++ run_wmt14_en_fr.sh
@@ -11,7 +11,7 @@ TASK="adv_translation"
 ARCH="adv_transformer_wmt_en_de"
 # Transformer Large
 #ARCH="adv_transformer_vaswani_wmt_en_de_big"
-	 
+
 MODEL_PATH="/home/nmt-research/repos/deepnmt/admin-data/models/wmt14-en-fr-admin-60-12l"
 LOG="/home/nmt-research/repos/deepnmt/admin-data/logs/wmt14-en-fr-admin-60-12l.log"
 INITF="/home/nmt-research/repos/deepnmt/admin-data/init_files/wmt14-en-fr-admin-60-12l.txt"
@@ -35,25 +35,24 @@ LR=0.001
 
 EPOCH=50
 
-
 # Generate scalars using only one gpu
 
-CUDA_VISIBLE_DEVICES=0 fairseq-train ${DATA_PATH} \
-       --arch ${ARCH} --task ${TASK} --adv-opt 0 --share-all-embeddings --optimizer radam \
-       --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt \
-       --warmup-init-lr 1e-07 --warmup-updates 8000 --lr ${LR} --min-lr 1e-09 \
-       --dropout 0.1 --attention-dropout 0.1 --relu-dropout 0.1 --weight-decay 0.0 \
-       --criterion adv_label_smoothed_cross_entropy --label-smoothing 0.1 \
-       --max-tokens $MAXTOKENS --update-freq $FRQ \
-       --fp16 --fp16-scale-window 256 --threshold-loss-scale 0.03125 \
-       --seed 1111 --restore-file x.pt --max-epoch ${EPOCH} --save-dir ${MODEL_PATH} \
-       --encoder-layers ${ELAYER} --decoder-layers ${DLAYER} \
-       --user-dir ${UDIR} --admin-init-type adaptive-profiling \
-       --admin-init-path ${INITF} \
-       --log-format simple --log-interval 100 | tee ${LOG}
+# CUDA_VISIBLE_DEVICES=0 fairseq-train ${DATA_PATH} \
+#        --arch ${ARCH} --task ${TASK} --adv-opt 0 --share-all-embeddings --optimizer radam \
+#        --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt \
+#        --warmup-init-lr 1e-07 --warmup-updates 8000 --lr ${LR} --min-lr 1e-09 \
+#        --dropout 0.1 --attention-dropout 0.1 --relu-dropout 0.1 --weight-decay 0.0 \
+#        --criterion adv_label_smoothed_cross_entropy --label-smoothing 0.1 \
+#        --max-tokens $MAXTOKENS --update-freq $FRQ \
+#        --fp16 --fp16-scale-window 256 --threshold-loss-scale 0.03125 \
+#        --seed 1111 --restore-file x.pt --max-epoch ${EPOCH} --save-dir ${MODEL_PATH} \
+#        --encoder-layers ${ELAYER} --decoder-layers ${DLAYER} \
+#        --user-dir ${UDIR} --admin-init-type adaptive-profiling \
+#        --admin-init-path ${INITF} \
+#        --log-format simple --log-interval 100 | tee ${LOG}
 
 # multi gpu training
-GPUS=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+GPUS=0,1
 CUDA_VISIBLE_DEVICES=$GPUS fairseq-train ${DATA_PATH} \
        --arch ${ARCH} --task ${TASK} --adv-opt ${AOPT} --share-all-embeddings --optimizer radam \
        --adam-betas "(0.9,0.98)" --clip-norm 0.0 --lr-scheduler inverse_sqrt \
